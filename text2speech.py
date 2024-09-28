614d35753b01bf2c72af38e774bd0273d039652e
simon0207 <simonsandeep4543@gmail.com>
2024-09-28 22:18:24 +0530

    Add files via upload

---
+++ text2speech.py
@@ -0,0 +1,34 @@
+import os
+
+from deepgram import (
+    DeepgramClient,
+    SpeakOptions,
+)
+
+
+filename = "output_final.wav"
+
+
+def text2speech(text):
+    try:
+        # STEP 1: Create a Deepgram client using the API key from environment variables
+        SPEAK_OPTIONS = {"text": text}
+        deepgram = DeepgramClient("############")
+
+        # STEP 2: Configure the options (such as model choice, audio configuration, etc.)
+        options = SpeakOptions(
+            model="aura-asteria-en",
+            encoding="linear16",
+            container="wav"
+        )
+
+        # STEP 3: Call the save method on the speak property
+        response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
+        return filename
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    text2speech("This is a test")
